80e3b0b84c3a5b9d9657c820ade84e0712af2a5d
Luis Gonzalez <luisgonzalez@Luiss-MacBook-Pro.local>
2022-04-04 21:50:10 -0400

    change sub header

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-
 const SubHeader = (props) => {
-	const history = useHistory()
-	const {newDate, setNewDate} = props
+  const history = useHistory();
+  const { newDate, setNewDate } = props;
 
-
-	
-	const handleChange =(e)=>{
-		setNewDate(e.target.value)
-		history.push('/search')
-	}
-
-
+  const handleChange = (e) => {
+    setNewDate(e.target.value);
+    history.push("/search");
+  };
 
   return (
     <div className="sub-header">
-      <input className="date-search" type='date' value={newDate} onChange={handleChange} placeholder = 'Search' size='5'/>
+      <div className="sub-content">
+        <h3>Search:</h3>
+        <input
+          className="date-search"
+          type="date"
+          value={newDate}
+          onChange={handleChange}
+          placeholder="Search"
+          size="5"
+        />
+      </div>
     </div>
   );
 };
