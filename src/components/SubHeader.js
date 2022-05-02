c93bbc3e7ce03b9b858bdd58cf6b81d80cd20df3
lmigonzalez <lmigonzalezs@gmail.com>
2022-05-01 23:46:06 -0400

    small fix

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -24,6 +24,7 @@ const SubHeader = (props) => {
           size="5"
         /> */}
         <InputGroup>
+        <h3>Search: </h3>
           <FormControl
             placeholder="Username"
             type="date"
